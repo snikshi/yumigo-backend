aaba59a4886183746db12e3abd17a5681eb3d1be
Shivarathri Nikhil <snikhil.nik@gmail.com>
2025-12-20 14:29:33 +0530

    Fixed User Schema definition

--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
-// The Blueprint
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // In real app, we encrypt this!
-  phone: { type: String, default: '' },
-  isGoldMember: { type: Boolean, default: false }, // For Yumigo Gold
+  password: { type: String, required: true },
+  phone: { type: String, default: "" },
+  isGoldMember: { type: Boolean, default: false },
   walletBalance: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
