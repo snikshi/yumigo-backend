0b59d6b9b379194fa0692f51a65b749093bfcb0e
Shivarathri Nikhil <snikhil.nik@gmail.com>
2025-12-20 14:21:30 +0530

    Converted User model to ES6

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // The Blueprint
 const UserSchema = new mongoose.Schema({
@@ -11,4 +11,4 @@ const UserSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+export default mongoose.model('User', userSchema);
